b0e469c339f1df473845f496384e57e225054999
226003 <226003@student.pwr.edu.pl>
2016-03-10 00:24:27 +0100

    Update gchart-pie.py

--- gchart-pie.py
+++ gchart-pie.py
@@ -1,15 +1,1 @@
-#!/usr/bin/python
 
-import sys
-
-labels = []
-values = []
-
-for line in sys.stdin:
-    value, label = line.strip().split(' ')
-    labels.append(label)
-    values.append(int(value))
-
-values = [value * 100 / sum(values) for value in values]
-
-print "http://chart.apis.google.com/chart?cht=p&chd=t:%s&chs=600x300&chl=%s" % ( ','.join(map(str,values)), '|'.join(labels))
